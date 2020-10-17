c361e8d9533ed04bbc006869df15d0ec6e395825
Daan Mooij <mail@daan-mooij.nl>
2020-10-17 13:27:40 +0200

    Firebase Setup

---
+++ Coderen/producthuntclone/src/firebase/config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC8WcHQwTkT4WNrz6uyRVIC-JO4bZTZT04",
+    authDomain: "producthuntclone-664b2.firebaseapp.com",
+    databaseURL: "https://producthuntclone-664b2.firebaseio.com",
+    projectId: "producthuntclone-664b2",
+    storageBucket: "producthuntclone-664b2.appspot.com",
+    messagingSenderId: "382666710575",
+    appId: "1:382666710575:web:a33bdcdbe25d44717ab0da",
+    measurementId: "G-7T0NP7ETHN"
+};
+
+export default firebaseConfig;
